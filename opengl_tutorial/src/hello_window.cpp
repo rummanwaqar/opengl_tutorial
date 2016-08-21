748206d58f7e0ac74a5a0a7726acf68e64ed902e
rummanwaqar <rumman.waqar05@gmail.com>
2016-08-20 18:17:42 -0700

    hello window example completed

--- opengl_tutorial/src/hello_window.cpp
+++ opengl_tutorial/src/hello_window.cpp
@@ -5,9 +5,14 @@
  *      Author: rumman
  */
 
+#include <iostream>
+
 #define GLEW_STATIC
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+// function prototypes
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 
 int main( int argc, char** argv )
 {
@@ -18,5 +23,53 @@ int main( int argc, char** argv )
 	glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
 	glfwWindowHint( GLFW_RESIZABLE, GL_FALSE );
 
+	// create a GLFW window object
+	GLFWwindow* window = glfwCreateWindow( 800, 600, "Hello Window", nullptr, nullptr );
+	if( window == nullptr )
+	{
+		std::cerr << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// set callbacks
+	glfwSetKeyCallback(window, key_callback);
+
+	// initialize glew before using any opengl functions
+	glewExperimental = GL_TRUE;
+	if( glewInit() != GLEW_OK )
+	{
+		std::cerr << "Failed to initialize GLEW" << std::endl;
+		return -1;
+	}
+
+	// set size of rendering window
+	int width, height;
+	glfwGetFramebufferSize( window, &width, &height );
+	glViewport( 0, 0, width, height );
+
+	// game loop
+	while( !glfwWindowShouldClose(window) )
+	{
+		glfwPollEvents();
+
+		// rendering commands here
+		glClearColor( 0.2f, 0.3f, 0.3f, 1.0f );
+		glClear( GL_COLOR_BUFFER_BIT );
+
+		glfwSwapBuffers( window );
+	}
+
+	// close shop
+	glfwTerminate();
 	return 0;
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	// when user presses esc close the window
+	if( key == GLFW_KEY_ESCAPE && action == GLFW_PRESS )
+		glfwSetWindowShouldClose( window, GL_TRUE );
+
 }
